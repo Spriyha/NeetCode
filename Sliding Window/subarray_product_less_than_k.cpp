f7d642c666137fd800c00944b72b7b6302a4f5b3
spriyha <mauryas00113@gmail.com>
2025-09-21 11:28:25 +0530

    subarray product less than k

---
+++ Sliding Window/subarray_product_less_than_k.cpp
@@ -0,0 +1,46 @@
+// Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+// Example 1:
+// Input: nums = [10,5,2,6], k = 100
+// Output: 8
+// Explanation: The 8 subarrays that have product less than 100 are:
+// [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+// Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+
+// Example 2:
+// Input: nums = [1,2,3], k = 0
+// Output: 0
+ 
+// Constraints:
+// 1 <= nums.length <= 3 * 104
+// 1 <= nums[i] <= 1000
+// 0 <= k <= 106
+
+//BruteForce 
+//TC : O(N^2)
+//SC : O(1)
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n = nums.size();
+        int res = 0;
+        for(int i = 0;i<n;i++){
+            int prod = 1;
+            for(int j = i;j<n;j++){
+                prod = prod * nums[j];
+                if(prod<k){
+                    res++;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
