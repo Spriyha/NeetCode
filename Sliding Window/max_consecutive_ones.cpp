a45d0756b99692621537c41a9eab2a5316e9fdd3
spriyha <mauryas00113@gmail.com>
2025-09-24 00:36:18 +0530

    max consecutive ones

---
+++ Sliding Window/max_consecutive_ones.cpp
@@ -0,0 +1,45 @@
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+// Example 1:
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+// Example 2:
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+ 
+// Constraints:
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+
+//1.BruteForce
+//TC : o(n^2)
+//SC : o(1)
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for(int i = 0 ; i<n;i++){
+            int count = 0 ;
+            for(int j = i ; j<n ; j++){
+                if(nums[j]==1){
+                    count++;
+                }
+                if(count == (j-i+1)){
+                    res = max(res,count);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+ 
