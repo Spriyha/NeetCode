808539bdf6e51f7355732bf88a06cc76796bee37
spriyha <mauryas00113@gmail.com>
2025-10-01 13:15:26 +0530

    Max num of vowel in a substring of given length

--- Sliding Window/maximum_number_of_vowel_in_a_substring_of_given_length.cpp
+++ Sliding Window/maximum_number_of_vowel_in_a_substring_of_given_length.cpp
@@ -49,3 +49,36 @@ public:
         return res;
     }
 };
+
+
+//2.Optimized
+//TC : O(N)
+//SC :O(1)
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        int n = s.size();
+        int i = 0,j = 0;
+        int count = 0;
+        int res = 0;
+        while(j<n){
+            if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'){
+                count++;
+            }
+            if((j-i+1)<k){
+                j++;
+            }
+            else if ((j-i+1)==k){
+                res = max(res,count);
+               if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                count--;
+                
+            }
+            i++;
+            j++;
+            }
+        }
+        return res;
+    }
+};
