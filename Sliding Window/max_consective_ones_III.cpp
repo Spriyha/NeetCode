13db8a9d4696da44a6e6f76198bf995d2fa0fdb0
spriyha <mauryas00113@gmail.com>
2025-10-08 23:48:58 +0530

    Max consective ones III

--- Sliding Window/max_consective_ones_III.cpp
+++ Sliding Window/max_consective_ones_III.cpp
@@ -50,3 +50,37 @@ public:
         return res;
     }
 };
+
+//2.Optimized
+//TC : O(N)
+//SC : O(1)
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0, j = 0;
+        int n = nums.size();
+        int res = 0;
+        int count = 0;
+        while(j<n){
+            if(nums[j]==0){
+                count++;
+            }
+            if(count <=k){
+                res = max(res,(j-i+1));
+                j++;
+            }
+            else{
+                while(count > k){
+                    if(nums[i]==0){
+                        count--;
+                    }
+                    i++;
+                }
+                res = max(res,(j-i+1));
+                j++;
+            }
+        }
+        return res;
+    }
+};
