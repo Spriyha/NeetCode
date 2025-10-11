30ee1f7d4b0d6951bc7fdae81e2e8df8b1382c89
spriyha <mauryas00113@gmail.com>
2025-10-12 01:00:26 +0530

    Subarray product less than k

--- Sliding Window/subarray_product_less_than_k.cpp
+++ Sliding Window/subarray_product_less_than_k.cpp
@@ -44,3 +44,36 @@ public:
         return res;
     }
 };
+
+//2.Optimized 
+//TC : O(N)
+//SC : O(1)
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i = 0,j = 0;
+        int  prod = 1;
+        int count = 0;
+        while(j<n){
+            prod *= nums[j];
+            if(prod < k){
+                count += (j-i+1);
+                j++;
+            }
+            else{
+                while(prod >= k && i<=j){
+                    prod /= nums[i];
+                    i++;
+                }
+                if(prod < k){
+                    count += (j-i+1);
+                }
+                j++;
+            }
+
+        }
+        return count;
+    }
+};
