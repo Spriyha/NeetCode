014fe7a46127842d03b76d43c089af66a845e27c
spriyha <mauryas00113@gmail.com>
2025-10-11 23:43:38 +0530

    Max sum of almost unique subarray

--- Sliding Window/maximum_sum_of_almost_unique_subarray.cpp
+++ Sliding Window/maximum_sum_of_almost_unique_subarray.cpp
@@ -51,3 +51,38 @@ public:
       return res;
     }
 };
+
+//2.Optimized 
+//TC : O(N)
+//SC : O(1)
+
+class Solution {
+public:
+    long long maxSum(vector<int>& nums, int m, int k) {
+        int i = 0, j=0;
+        int n = nums.size();
+        long long res = 0;
+        long long sum = 0;
+        map<int,int>freq;
+        while(j<n){
+            freq[nums[j]]++;
+            sum =  sum + nums[j];
+            if((j-i+1)<k){
+                j++;
+            }
+            else if ((j-i+1)==k){
+                if(freq.size()>=m){
+                    res = max(res,sum);
+                }
+                sum -= nums[i];
+                freq[nums[i]]--;
+                if (freq[nums[i]] == 0){
+                    freq.erase(nums[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return res;
+    }
+};
